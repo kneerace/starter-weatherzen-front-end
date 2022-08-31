2c5a76fcf88155c6c047c633f49d66b81d011ae5
kneerace <kneerace@gmail.com>
2022-08-31 05:13:13 -0400

    f

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,48 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5050";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
+ const headers = new Headers();
+ headers.append("Content-Type", "application/json");
+
+// Creating Observation
+/* below is calling internally which happens in instance only 
+const observations =[];
+
+function nextId(){
+  const unit32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+  return unit32.toString(16);
+}
+
+export async function createObservation(observation, signal){
+  const now = new Date().toISOString();
+  console.log("createObservations::: ", nextId);
+  const newObservation ={ ...observation, 
+  observation_id: nextId(), 
+  created_at: now, 
+  updated_at: now, 
+};
+observations.push(newObservation);
+return newObservation;
+}
+
+*/
+// below is calling API
+export async function createObservation(observation, signal){
+  const url = `${API_BASE_URL}/observations`;
+  const options={method:"POST", 
+        headers, 
+        body: JSON.stringify({data: observation}),
+        signal,
+      }
+
+return await fetchJson(url, options);
+}
+
 
 /**
  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
@@ -43,4 +78,12 @@ async function fetchJson(url, options) {
       return Promise.reject({ message: error.message });
     }
   }
+}
+
+// listing Observation
+export async function listObservations(signal){
+  // return [];
+  const url = `${API_BASE_URL}/observations`;
+  const options ={headers, signal, };
+  return await fetchJson(url, options);
 }
